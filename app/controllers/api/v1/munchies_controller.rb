6c280ffaf5e8cc96c89d1e391f3b37fefd815163
Brian Plantico <bplantico@gmail.com>
2019-07-29 10:27:05 -0600

    Adds connection to Yelp API and is returning 3 restaurants taking into account the food term and restaurants being open at 'x' time

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -10,6 +10,29 @@ class Api::V1::MunchiesController < ApplicationController
       faraday.adapter Faraday.default_adapter
     end
     directions_response = directions_conn.get("/maps/api/directions/json")
-    trip_duration = JSON.parse(directions_response.body, symbolize_names: true)[:routes][0][:legs][0][:duration][:value]
+    result = JSON.parse(directions_response.body, symbolize_names: true)[:routes][0][:legs][0]
+
+    # Prepping params to send to YELP API
+    trip_duration = result[:duration][:value]
+    destination_coords = result[:end_location]
+    end_lat = destination_coords[:lat]
+    end_lng = destination_coords[:lng]
+    leave_at = Time.now
+    arrival_time = (leave_at + trip_duration).to_i
+    food_type = params[:food]
+
+    # GREAT! SEND THESE TO YELP FTW
+
+    yelp_conn = Faraday.new(:url => 'https://api.yelp.com') do |faraday|
+      faraday.params['latitude'] = end_lat
+      faraday.params['longitude'] = end_lng
+      faraday.params['limit'] = 3
+      faraday.params['open_at'] = arrival_time
+      faraday.params['term'] = food_type
+      faraday.adapter Faraday.default_adapter
+    end
+    yelp_conn.authorization :Bearer, ENV['YELP_API_KEY']
+    yelp_response = yelp_conn.get("/v3/businesses/search")
+    require "pry"; binding.pry
   end
 end
