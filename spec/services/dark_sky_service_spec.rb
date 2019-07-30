8600c8f15ac8fcecacbf3c566150dd30fe75ab9f
Brian Plantico <bplantico@gmail.com>
2019-07-30 09:26:23 -0600

    Adds dark sky service spec. Testing that I receive a five day forecast back from dark sky that returns a day and daily summary

---
+++ spec/services/dark_sky_service_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe DarkSkyService do
+  it "gets five day forecast with day and summary from dark sky api" do
+    service = DailyForecastService.new
+    forecast = service.get_five_day_forecast('denver,co')
+    expect(forecast.class).to eq(Array)
+  end
+end
