5f1be857710983bf9556beabdb2becdb38762388
Brian Plantico <bplantico@gmail.com>
2019-07-30 10:08:23 -0600

    Refactors daily_forecast_service_spec. I am now passing in a named location and the daily forecast service instantiates an instance of the location coordinate service spec to return coords which are passed to dark sky's api

--- spec/services/daily_forecast_service_spec.rb
+++ spec/services/daily_forecast_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe DailyForecastService do
   it "gets five day forecast with day and summary given location coords" do
     service = DailyForecastService.new
-    location = {:lat=>39.7392358, :lng=>-104.990251}
+    location = 'denver,co'
     forecast = service.get_five_day_forecast(location)
     expect(forecast.class).to eq(Array)
   end
