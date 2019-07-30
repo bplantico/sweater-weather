a5996b0cd4588315e5fd0c689b6a191faf4fce36
Brian Plantico <bplantico@gmail.com>
2019-07-30 09:31:04 -0600

    Renames daily forecast service spec to be more generic in case we opt not to use dark sky in the future

--- spec/services/daily_forecast_service_spec.rb
+++ spec/services/daily_forecast_service_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe DarkSkyService do
+RSpec.describe DailyForecastService do
   it "gets five day forecast with day and summary from dark sky api" do
     service = DailyForecastService.new
     forecast = service.get_five_day_forecast('denver,co')
