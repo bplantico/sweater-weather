1440473c1855000fe380cc743851db435273924a
Brian Plantico <bplantico@gmail.com>
2019-07-30 09:23:51 -0600

    Changes giphy spec for api request endpoint

--- spec/api/v1/giphy_image_request_spec.rb
+++ spec/api/v1/giphy_image_request_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 describe "Giphy image API" do
   it "sends giphy image for a given search term" do
-    get '/api/v1/giphy?q=denver,co'
+    get '/api/v1/gifs?location=denver,co'
     expect(response).to be_successful
 
     result = JSON.parse(response.body)
+
+    # require "pry"; binding.pry
   end
 end
