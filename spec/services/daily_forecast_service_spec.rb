c8c84fa684b157f5877a6a05cb70870b359412cb
Brian Plantico <bplantico@gmail.com>
2019-07-30 09:43:07 -0600

    Refactors daily forecast service spec to take in location in the form of lat/long coords instead of a named location since Dark Sky api takes coords and not named places

--- spec/services/daily_forecast_service_spec.rb
+++ spec/services/daily_forecast_service_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DailyForecastService do
-  it "gets five day forecast with day and summary from dark sky api" do
+  it "gets five day forecast with day and summary given location coords" do
     service = DailyForecastService.new
-    forecast = service.get_five_day_forecast('denver,co')
+    location = {:lat=>39.7392358, :lng=>-104.990251}
+    forecast = service.get_five_day_forecast(location)
     expect(forecast.class).to eq(Array)
   end
 end
