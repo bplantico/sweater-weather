7eda871ec1a2a9e2fb58de107780c4a8763f9b1b
Brian Plantico <bplantico@gmail.com>
2019-07-31 09:49:26 -0600

    Changes 'get_duration' method call to be a class method instead of instance method

--- app/services/google_directions_service.rb
+++ app/services/google_directions_service.rb
@@ -1,5 +1,5 @@
 class GoogleDirectionsService
-  def get_duration(origin, destination)
+  def self.get_duration(origin, destination)
     directions_response = directions_conn(origin, destination).get("/maps/api/directions/json")
     directions_result = JSON.parse(directions_response.body, symbolize_names: true)
     time_in_sec = directions_result[:routes][0][:legs][0][:duration][:value]
@@ -7,7 +7,7 @@ class GoogleDirectionsService
 
   private
 
-  def directions_conn(origin, destination)
+  def self.directions_conn(origin, destination)
     Faraday.new(:url => 'https://maps.googleapis.com') do |faraday|
       faraday.params['key'] = ENV['GOOGLE_GEOCODE_API_KEY']
       faraday.params['origin'] = origin
