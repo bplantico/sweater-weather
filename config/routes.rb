96242294252f6eb7e7b3a9237496936e8e8c1cda
Brian Plantico <bplantico@gmail.com>
2019-07-31 10:07:23 -0600

    Adds route for root path and welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  
   namespace :api do
     namespace :v1 do
         get '/forecast',    to: 'forecast#show'
@@ -12,5 +14,3 @@ Rails.application.routes.draw do
     end
   end
 end
-
-# [POST] "/api/v1/road_trip"
