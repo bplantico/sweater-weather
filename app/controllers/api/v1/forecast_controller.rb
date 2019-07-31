fa371546edca18afff37c83da65d9985c6ddae57
Brian Plantico <bplantico@gmail.com>
2019-07-31 12:46:32 -0600

    Refactors forecast controller to make call to location coords service. Next refactor would be adding a facade that would take care of making the calls so all the controller does is renders the json through a serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,13 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
 
   def show
-
     lat_long = LocationCoordinatesService.new.get_coordinates(params[:location])
     latitude = lat_long[:lat]
     longitude = lat_long[:lng]
     dark_sky_response = Faraday.get "https://api.darksky.net/forecast/#{ENV['DARK_SKY_SECRET_KEY']}/#{latitude},#{longitude}"
     dark_sky_forecast = JSON.parse(dark_sky_response.body, symbolize_names: true)
-
     render json: ForecastSerializer.new(dark_sky_forecast, params[:location]).all_details
   end
 end
